9d7ec6c3314fd4804488c7cb72ad28d4e1008944
Thalles <thallesgabriel1307@gmail.com>
2020-12-01 13:23:43 -0300

    master

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+    async create({ request }) {
+        const data = request.only(['username', 'email', 'password'])
+
+        console.log(data)
+
+        const user = await User.create(data);
+
+        console.log(user)
+
+        return user
+    }
+}
+
+module.exports = UserController
