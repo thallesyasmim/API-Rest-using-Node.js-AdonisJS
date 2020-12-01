050713880e9094bf499dff7172235a5f0181d7eb
Thalles <thallesgabriel1307@gmail.com>
2020-12-01 17:02:53 -0300

    Upload In Images

--- app/Models/Image.js
+++ app/Models/Image.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Image extends Model {
+    static get computed()  {
+        return ['url']
+    }
+
+    getUrl({ path }) {
+        return `${Env.get('APP_URL')}/images/${path}`
+    }
+
 }
 
 module.exports = Image
